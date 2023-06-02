aa72ee54631b58e46783ab580ec7d5f5b0367ccb
86159 <zxw3180716935@163.com>
2023-06-02 18:17:03 +0800

    add

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
 - [test](test) &#x2002; 测试代码
   - [初始化类程序](test/Init)
     - [LED 灯闪烁及串口输出](test/Init/LedBlinkAndSerialOut.cpp)
+  - [通信类程序](test/Communication) &#x2002; 与 ArduinoNano 通信运行的程序，程序另一半见 ArduinoNano 项目同名程序
+    - [LED 闪烁及串口通信](test/Communication/LedBlinkAndSerial.cpp)
+    - 
--- test/Communication/LedBlinkAndSerial.cpp
+++ test/Communication/LedBlinkAndSerial.cpp
@@ -7,7 +7,8 @@ void setup() {
 }
 
 void loop() {
-    int i = digitalRead(13);   // 滤杂波
+//    int i = analogRead(13)/1023;   // 模拟信号读取需要滤杂波
+    int i = digitalRead(13);   // 数字信号读取无须滤杂波
     digitalWrite(BLUE_LED,i);
     Serial.println(i);
 //    delay(1000);   // 有 三角波   无 方波（占空比不是 0.5 且低电平存在杂波，滤去后正常）
